3b2c4461ac0ff356f454f2d17c1a13248a43e99b
MikhailKuptsov <91967841+MikhailKuptsov@users.noreply.github.com>
2025-03-01 13:12:45 +0300

    Add files via upload

---
+++ src/auth_page/auth_page.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Auth from "./Auth";
+// import '../styles/styles_auth.css'
+
+
+export default class Auth_page extends Component{
+    render(){
+        return (
+            <div >
+                <Auth />
+            </div>
+        );
+    }
+    
+};
